fb9bf97b4187f7c804064890f3857e31c6f2c88e
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-07-01 20:00:47 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main- main block
+ * Description: prints the alphabet in lowercase
+ * except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != e && c != q)
+			putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
