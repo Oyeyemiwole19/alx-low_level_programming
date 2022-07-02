2abbd44c711d36fc877c1b6d6dca534f7a861885
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-07-01 19:15:38 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * Description: prints alphabet in lowercase
+ * and then uppercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c  = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	c = 'A'
+		while (c <= 'Z')
+		{
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
