3ef1c421e80b764cbadee4b983733e65103504e2
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-07-01 20:21:28 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- main block
+ * Desciption: print single digit number
+ * base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10 ; a++)
+		printf("%d", a);
+	printf('\n');
+	return (0);
+}
