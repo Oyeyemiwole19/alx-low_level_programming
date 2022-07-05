96d35397d542d5366c1232447c0f11c61cf6cde5
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-07-05 09:07:29 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet -> print the lowercase alphabets
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
