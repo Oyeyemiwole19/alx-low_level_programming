48ba68ee70a063ee953055fdfb48476fd2052566
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-07-05 14:42:28 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - return the absolute value of an integer
+ * @n: n is the parameter
+ * Return: return the absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
