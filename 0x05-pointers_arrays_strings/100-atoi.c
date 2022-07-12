968e30f2afb5292d182bfb01c191969399149542
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-07-12 11:14:11 -0700

    Task 10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _atoi- convert a string to an integer
+ * @s: the string to be converted.
+ * Return: value of the converted string
+ */
+int _atoi(char *s)
+{
+	int lis = 1;
+	unsigned int a = 0;
+
+	do {
+		if (*s == '-')
+			lis *= -1;
+		else if (*s >= '0' && <= '9')
+			a = (a * 10) + (*s - '0');
+		else if (a > 0)
+			break;
+	} while (*s++);
+
+	return (a * lis);
