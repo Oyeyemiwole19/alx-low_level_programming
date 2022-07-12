e9adcf66d38745c146c72131b369b595a86e4191
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-07-12 09:22:23 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+i#include "main.h"
+/**
+ * rev_string - reverses a string.
+ * @s: string to reverse
+ */
+void rev_string(char *s)
+{
+	int len, i, les;
+	char temp;
+
+	for (len = 0; s[len] != '\0' len++)
+		i = 0;
+	les = len / 2;
+	while (les--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
+}
