50e8ebb4a4f00cb614cdd7c79737a644216bb127
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-07-22 14:28:18 -0700

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio/h>
+/**
+ * main- main function
+ * @argc: offset
+ * @argv: offset value
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
