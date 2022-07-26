b6e582a1582f188c154628660f9a96faf52f89f6
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-07-26 05:14:25 -0700

    3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * **alloc_grid- prints a grid
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: A pointer
+ */
+int **alloc_grid(int width, int height)
+{
+	int **s, i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	s = malloc(sizeof(int *) * height);
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		s[i] = malloc(sizeof(int) * width);
+		if (s[i] == NULL)
+		{
+			for (; i >= 0; i--)
+			{
+				free(s[i]);
+			}
+			free(s);
+			return (NULL);
+		}
+		for (j = 0; j <= width; j++)
+		{
+			s[i][j] = 0;
+		}
+	}
+	return (s);
+}
