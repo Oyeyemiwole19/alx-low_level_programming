b090493cbe9b179a2bebf8a4144d603267d0ff7e
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-07-25 17:09:00 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,11 +11,22 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
+	{
 		return (NULL);
-	arr = malloc(sizeof(char) * size);
-	if (arr == NULL)
+	}
+	arr = (char *) malloc(size * sizeof(c));
+	if (arr == 0)
+	{
 		return (NULL);
-	for (i = 0; i < size; i++)
-		arr[i] = c;
-	return (arr);
+	}
+	else
+	{
+		i = 0;
+		while (i < size)
+		{
+			*(arr + i) = c;
+			i++;
+		}
+		return (arr)
+	}
 }
