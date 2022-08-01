79730c8564856325ff967d2de57e5149cf5574cb
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-08-01 07:07:49 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog- A new type definig a dog
+ * @name: The name of the dpg
+ * @age: The dog age
+ * @owner: The dog owner
+ */
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
