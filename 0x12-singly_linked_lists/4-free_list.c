62b5cc5ebddcab050d5ebf294f3f51f437b2bfac
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-08-11 18:42:30 -0700

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_list- frees list
+ * @head: A pointer
+ */
+void free_list(list_t *head)
+{
+	list_t *t;
+
+	while (head)
+	{
+		t = head->next;
+		free(head->str);
+		free(head);
+		head = t;
+	}
+}
