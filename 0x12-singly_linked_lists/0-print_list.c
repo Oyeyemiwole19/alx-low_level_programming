cf70522c2211fee7f28f0dced91bbd4385141938
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-08-11 19:56:59 -0700

    0.Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * print_list- prints all the elements of a list_t list.
+ * print_list- prints all the elements of a linked list
  * @h: singly linked list
- * Return: number of elements in the list
+ * Return: number of elements in the list.
  */
 size_t print_list(const list_t *h)
 {
