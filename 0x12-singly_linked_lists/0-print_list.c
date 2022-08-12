c6441912484de912200cafbe43135b781c42dacb
Oyeyemi Toheeb <opeyemioyeyemi141@gmail.com>
2022-08-11 20:01:57 -0700

    0.print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,14 @@ size_t print_list(const list_t *h)
 	size_t nelem;
 
 	nelem = 0;
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
 		nelem++;
+		h = h->next;
 	}
 	return (nelem);
 }
